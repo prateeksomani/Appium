8fe9d088b4e0d5fd4905c2c5308fe3ec292fae08
prateek <prateek@prateek-PC.mshome.net>
2018-08-14 17:02:57 +0530

    extent and log reporting also implemented desired capability

--- Appium/src/main/java/com/test/appium/testbase/TestBase.java
+++ Appium/src/main/java/com/test/appium/testbase/TestBase.java
@@ -1,20 +1,79 @@
 package com.test.appium.testbase;
 
-import org.testng.annotations.Test;
-import org.testng.annotations.BeforeTest;
-import org.testng.annotations.AfterTest;
+import java.lang.reflect.Method;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.ExtentTest;
+import com.relevantcodes.extentreports.LogStatus;
+
+
 
 public class TestBase {
-  @Test
-  public void f() {
-	  System.out.println("hello");
-  }
-  @BeforeTest
-  public void beforeTest() {
-  }
-
-  @AfterTest
-  public void afterTest() {
-  }
-
+	public static ExtentReports extentReports;
+	public static ExtentTest test;
+	public WebDriver driver;
+	public static Logger log=Logger.getLogger(TestBase.class.getName());
+	static {
+		Calendar calendar=Calendar.getInstance();
+		SimpleDateFormat dateFormat=new SimpleDateFormat("dd_MM_yyyy_hh_mm_ss");
+		extentReports=new ExtentReports(System.getProperty("user.dir")+"/src/main/java/ExecutionReport/test"+dateFormat.format(calendar.getTime())+".html",false);//false =previous reports will not be deleted
+	}
+	public void getExecutionResult(ITestResult result) {
+		if (result.getStatus()==ITestResult.SUCCESS) {
+			test.log(LogStatus.PASS,result.getName(), "test is pass");
+		} else if(result.getStatus()==ITestResult.FAILURE) {
+			test.log(LogStatus.FAIL,result.getName(), "test is fail");
+		}else if(result.getStatus()==ITestResult.SKIP) {
+			test.log(LogStatus.SKIP,result.getName(), "test is SKIP and skip reason is :"+result.getThrowable());
+		}else if(result.getStatus()==ITestResult.STARTED) {
+			test.log(LogStatus.INFO,result.getName(), "test is started");
+		}
+	}
+	public void init() throws MalformedURLException{
+		setCapability();
+		String log4jConfPath="log4j.properties";
+		PropertyConfigurator.configure(log4jConfPath);
+	}
+	public void setCapability() throws MalformedURLException{
+		DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("deviceName","emulator-5554");
+        capabilities.setCapability("platformName","Android");
+        capabilities.setCapability("appPackage", "");
+        capabilities.setCapability("appActivity", "");
+        
+        driver = new RemoteWebDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);           
+		
+	}
+	@BeforeMethod
+	public void beforeMethod(Method method) {
+		test=extentReports.startTest(method.getName());
+		test.log(LogStatus.INFO, method.getName()+" test started");
+	}
+	@AfterMethod
+	public void afterMethod(ITestResult iTestResult) {
+		getExecutionResult(iTestResult);
+	}
+	@AfterClass(alwaysRun=true)
+	public void endtest() {
+		extentReports.endTest(test);
+		extentReports.flush();
+	}
 }
+
